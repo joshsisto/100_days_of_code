3439d6ace003cfc62944e1dff23b02de4732948b
Josh Sisto <joshsisto@gmail.com>
2018-06-13 14:31:26 -0700

    Created day5.py

---
+++ code/days_4-6/day5.py
@@ -0,0 +1,41 @@
+from collections import defaultdict, namedtuple, Counter, deque
+import csv
+import random
+from urllib.request import urlretrieve
+
+
+movie_data  = 'https://raw.githubusercontent.com/pybites/challenges/solutions/13/movie_metadata.csv'
+movies_csv = 'movies.csv'
+urlretrieve(movie_data, movies_csv)
+
+Movie = namedtuple('Movie', 'title year score')
+
+def get_movies_by_director(data=movies_csv):
+    """Extracts all movies from csv and stores them in a dictionary
+       where keys are directors, and values is a list of movies (named tuples)"""
+    directors = defaultdict(list)
+    with open(data, encoding='utf-8') as f:
+        for line in csv.DictReader(f):
+            try:
+                director = line['director_name']
+                movie = line['movie_title'].replace('\xa0', '')
+                year = int(line['title_year'])
+                score = float(line['imdb_score'])
+            except ValueError:
+                continue
+
+            m = Movie(title=movie, year=year, score=score)
+            directors[director].append(m)
+
+    return directors
+
+
+directors = get_movies_by_director()
+
+print(directors['Christopher Nolan'])
+
+cnt = Counter()
+for director, movies in directors.items():
+    cnt[director] += len(movies)
+
+cnt.most_common(5)
