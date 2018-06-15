bcc2bbaa848c02b5d01936d3db85fe293c1cd1b1
Josh Sisto <joshsisto@gmail.com>
2018-06-15 10:37:48 -0700

    Day6 working for baseball
    Still need to work on this

--- code/days_4-6/day6.py
+++ code/days_4-6/day6.py
@@ -1,8 +1,36 @@
 from collections import defaultdict, namedtuple, Counter, deque
 import csv
+import collections
+
+Player = collections.namedtuple('Players', 'player, team, position, height, weight, age, pc')
 
 baseball_csv = './baseball.csv'
 fifa_csv = './fifa.csv'
 
 
+
+def get_player_stats(path):
+    with open(path, encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+
+        player_dict = collections.defaultdict(list)
+        for line in reader:
+            try:
+                player = line['Name']
+                team = line['Team']
+                position = line['Position']
+                height = line['Height']
+                weight = line['Weight']
+                age = line['Age']
+                pc = line['PosCategory']
+            except ValueError:
+                continue
+
+            p = Player(player=player, team=team, position=position, height=height, weight=weight, age=age, pc=pc)
+            player_dict[player].append(p)
+
+        return player_dict
+
+
+print(get_player_stats(baseball_csv))
 
