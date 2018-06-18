644e368711578183dcf89d21493022b2c237d17f
Josh Sisto <joshsisto@gmail.com>
2018-06-18 09:59:35 -0700

    Starting day 9

--- code/07-09-data-structures/day8_official.py
+++ code/07-09-data-structures/day8_official.py
@@ -1,4 +1,3 @@
-## https://codechalleng.es/bites/21/
 
 cars = {
     'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
@@ -8,49 +7,39 @@ cars = {
     'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
 }
 
-
 def get_all_jeeps():
     """return a comma separated string of jeep models (original order)"""
-    # l = cars['Jeep']
-    # for vehicle in l:
-    #     print(str(vehicle))
-    # print(str(cars['Jeep']))
-    return (str(cars['Jeep']))
-    pass
+    print(', '.join(cars['Jeep']))
+    return ', '.join(cars['Jeep'])
 
 
 def get_first_model_each_manufacturer():
     """return a list of matching models (original ordering)"""
-    first_car = []
-    for k, v in cars.items():
-        # print(v[0])
-        first_car.append(v[0])
-    print(first_car)
-    return first_car
-    pass
+    print([models[0] for models in cars.values()])
+    return [models[0] for models in cars.values()]
 
 
 def get_all_matching_models(grep='trail'):
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
-    trail_list = []
-    for k, v in cars.items():
-        for i in v:
-            if "Trail" in i:
-                trail_list.append(i)
-    print(trail_list)
-    return trail_list
-    pass
+    grep = grep.lower()
+    models = sum(cars.values(), [])  # flatten list of lists
+    matching_models = [model for model in models
+                       if grep in model.lower()]
+    print(sorted(matching_models))
+    return sorted(matching_models)
 
 
 def sort_car_models():
     """sort the car models (values) and return the resulting cars dict"""
-    pass
+    print({manufacturer: sorted(models) for
+            manufacturer, models in cars.items()})
+    return {manufacturer: sorted(models) for
+            manufacturer, models in cars.items()}
 
 
 get_all_jeeps()
 get_first_model_each_manufacturer()
 get_all_matching_models()
 sort_car_models()
-
