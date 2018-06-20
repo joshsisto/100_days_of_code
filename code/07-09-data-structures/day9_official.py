d5f3389910b199a0e3a2ebca58e1bdac6f15c6bb
Josh Sisto <joshsisto@gmail.com>
2018-06-20 11:22:31 -0700

    Started Day10

---
+++ code/07-09-data-structures/day9_official.py
@@ -0,0 +1,63 @@
+us_state_abbrev = {'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ',
+                   'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO',
+                   'Connecticut': 'CT', 'Delaware': 'DE', 'Florida': 'FL',
+                   'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID',
+                   'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA',
+                   'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA',
+                   'Maine': 'ME', 'Maryland': 'MD', 'Massachusetts': 'MA',
+                   'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS',
+                   'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE',
+                   'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ',
+                   'New Mexico': 'NM', 'New York': 'NY',
+                   'North Carolina': 'NC', 'North Dakota': 'ND',
+                   'Ohio': 'OH', 'Oklahoma': 'OK', 'Oregon': 'OR',
+                   'Pennsylvania': 'PA', 'Rhode Island': 'RI',
+                   'South Carolina': 'SC', 'South Dakota': 'SD',
+                   'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT',
+                   'Vermont': 'VT', 'Virginia': 'VA', 'Washington': 'WA',
+                   'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'}
+
+states = ['Oklahoma', 'Kansas', 'North Carolina', 'Georgia', 'Oregon',
+          'Mississippi', 'Minnesota', 'Colorado', 'Alabama',
+          'Massachusetts', 'Arizona', 'Connecticut', 'Montana',
+          'West Virginia', 'Nebraska', 'New York', 'Nevada', 'Idaho',
+          'New Jersey', 'Missouri', 'South Carolina', 'Pennsylvania',
+          'Rhode Island', 'New Mexico', 'Alaska', 'New Hampshire',
+          'Tennessee', 'Washington', 'Indiana', 'Hawaii', 'Kentucky',
+          'Virginia', 'Ohio', 'Wisconsin', 'Maryland', 'Florida',
+          'Utah', 'Maine', 'California', 'Vermont', 'Arkansas', 'Wyoming',
+          'Louisiana', 'North Dakota', 'South Dakota', 'Texas',
+          'Illinois', 'Iowa', 'Michigan', 'Delaware']
+
+NOT_FOUND = 'N/A'
+
+
+def get_every_nth_state(n=10):
+    """Return a list with every nth item (default 10th item)
+       of states (takeaway: lists keep order)"""
+    new_states = []
+    for i, state in enumerate(states, 1):
+        if i % n == 0:
+            new_states.append(state)
+    return new_states
+
+
+def get_state_abbrev(abbrev):
+    """Look up a state abbreviation by full name in
+       us_state_abbrev, if not found return NOT_FOUND
+       (takeaway: dicts are great for lookups)"""
+    return us_state_abbrev.get(abbrev) or NOT_FOUND
+
+
+def get_longest_state(data):
+    """Takes dict or list and determines the longest state name
+       (takeaways: use a dict method to get all keys, use sorted)"""
+    return max(data, key=len)
+
+
+def combine_state_names_and_abbreviations():
+    """Return a new list with the first 10 abbreviations from the
+       us_state_abbrev dict ordered values and the last 10 states from
+       the states list (takeaways: use another dict method to get all
+       values and use sorted, list slicing and list concatenation)"""
+    return sorted(us_state_abbrev.values())[:10] + sorted(states)[-10:]
