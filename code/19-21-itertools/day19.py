24ea96bacf56aaa9a26f433b6c0cca2962f2788d
llamafarmer <joshsisto@gmail.com>
2018-07-08 17:54:34 -0700

    day19
    Its cracking

---
+++ code/19-21-itertools/day19.py
@@ -0,0 +1,43 @@
+number = list(range(1, 11))
+
+print(number)
+
+for i in number:
+    print(i)
+
+print('__iter__' in dir("number"))
+
+it = iter('string')
+
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+# print(next(it))  # This will cause error because it can't be iterated on again
+
+import itertools
+import sys
+import time
+
+symbols = itertools.cycle('-\|/')
+
+# while True:
+#     sys.stdout.write('\r' + next(symbols))
+#     sys.stdout.flush()
+#     time.sleep(1)
+
+
+from itertools import product
+
+name = 'joshua'
+
+for letter in product(name, repeat=1):
+    print(letter)
+
+for letter in product(name, repeat=5):
+    print(letter)
+
+
+ 
