cef5947d8e2f0c4b64c35351c674a620910df9f5
llamafarmer <joshsisto@gmail.com>
2018-07-24 19:24:59 -0700

    Upload day 26 and 27
    Guess I forgot to upload these

--- code/25-27-error-handling/day27.py
+++ code/25-27-error-handling/day27.py
@@ -1,5 +1,7 @@
 # https://medium.freecodecamp.org/creating-a-twitter-bot-in-python-with-tweepy-ac524157a607
 # https://www.digitalocean.com/community/tutorials/how-to-create-a-twitterbot-with-python-3-and-the-tweepy-library
+# https://dototot.com/reply-tweets-python-tweepy-twitter-bot/
+# https://stackoverflow.com/questions/38872195/tweepy-exclude-retweets
 
 from __future__ import absolute_import, print_function
 from datetime import datetime, timedelta
@@ -8,16 +10,15 @@ import tweepy
 import requests
 from time import sleep
 
-consumer_key = 'v6PnUc1baJleHQOU0PLoQrjJh'
-consumer_secret = 'VpnPIGCNBjQQaK6muMRkMyxmtGtjhdrulYwwaBy8EfhIOspKnp'
-access_token = '798343103178358784-19IECbagBM2pzf1KjvqIlg81sVQn08q'
-access_token_secret = 'S3nbsV0TnA4ewN0TAHKNT6M4qMWG4IDqR8MArcPtucD5l'
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # my_tweet = input("What would you like to tweet")
+
+# twts = api.search()
 
 ignore_list = ["_30days30sites", "100_DaysOfCode", "100_DaysOfCode_", "100DaysOfCode_", "_100DaysOfCode", "Evil_1T",
                "lancetay", "DaysofcodeNg", "rogue_corq", "muthiimm", "GitLit000", "gadgetgirlcodes", "TheDevelBot",
@@ -26,30 +27,35 @@ ignore_list = ["_30days30sites", "100_DaysOfCode", "100_DaysOfCode_", "100DaysOf
 
 def retweet_like_follow(hashtag):
     """Retweet, Like, and Follow users based off hashtag"""
-    for tweet in tweepy.Cursor(api.search, q=hashtag).items():
+    for tweet in tweepy.Cursor(api.search, q=hashtag + " -filter:retweets", tweet_mode='extended').items():
         try:
+            # ignore users on the ignore list
             if tweet.user.screen_name in ignore_list:
                 continue
-            # Add \n escape character to print() to organize tweets
+            # Print user and tweet
             print('\nTweet by: @' + tweet.user.screen_name)
-            sleep(5)
+            # print("\n" + str(tweet))
+            # print(type(tweet))
+            print(tweet.full_text)
+            # print(tweet.extended_tweet)
+            sleep(1)
 
             # Retweet tweets as they are found
             tweet.retweet()
             print('Retweeted the tweet')
-            sleep(5)
+            sleep(2)
 
             # Favorite the tweet
             tweet.favorite()
             print('Favorited the tweet')
-            sleep(5)
+            sleep(1)
 
+            # Follow the user if you aren't already
             if not tweet.user.following:
-                # Don't forget to indent
                 tweet.user.follow()
                 print('Followed the user')
 
-            sleep(380)
+            sleep(15)
         except ConnectionResetError:
             break
         except tweepy.TweepError as e:
